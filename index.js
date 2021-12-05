e22b9d5fcdb16bbc0b7aec67c2cc69235fc9a104
Inuch <90455677+Inuch@users.noreply.github.com>
2021-12-05 16:06:47 -0300

    If you don't get the chart method, use the slice

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function capitalize(str) {
 
 // Alternative Solution
 const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+
+
+//Other solution
+
+function capitalize(str) {
+    return str.slice(0, 1).toUpperCase() + str.slice(1);
+}
